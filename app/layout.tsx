dd8a18ab77956d8c6feb460c12a65f974bcba7b2
cc <31040856+CCleanerShot@users.noreply.github.com>
2023-09-19 02:32:24 -0500

    stripe integration added

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import UserProvider from "@/providers/UserProvider";
 import ModalProvider from "@/providers/ModalProvider";
 import ToasterProvider from "@/providers/ToasterProvider";
 import getSongsByUserId from "@/actions/getSongsByUserId";
+import getActiveProductsWithPrices from "@/actions/getActiveProducts";
 
 import Player from "@/components/Player";
 
@@ -20,6 +21,7 @@ export const metadata: Metadata = {
 export const revalidate = 0;
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
 	const userSongs = await getSongsByUserId();
+	const products = await getActiveProductsWithPrices();
 
 	return (
 		<html lang="en">
@@ -27,7 +29,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
 				<ToasterProvider />
 				<SupebaseProvider>
 					<UserProvider>
-						<ModalProvider />
+						<ModalProvider products={products} />
 						<Sidebar songs={userSongs}>{children}</Sidebar>
 						<Player />
 					</UserProvider>
